4cedcc858954507e60f614e9a434db7bbc8096c2
halaway <31904474+halaway@users.noreply.github.com>
2024-10-04 18:04:52 -0400

    updating permissions

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 <h1 align="center"> Quick Fill </h1>
-A quick and easy Chrome Extension tool for creating Workday accounts that auto-fills its registration form with pre-saved user information.
+A quick and easy Chrome Extension tool for both signing in and creating a Workday account that auto-fills its registration form with pre-saved user information.
+
 <img width="700" alt="image-browser" src="https://github.com/user-attachments/assets/27a6fd61-c4b2-40d5-9eb1-94e8b22a1278">
 
 # Installation
-Soon available on the Chrome Web Extension Store.
-Although, to install, simply fork the reposistory and ```cd/Quick-Fill``` directory.
+To install, simply fork the reposistory and ```cd/Quick-Fill``` directory.
 ```
 git clone https://github.com/halaway/Quick-Fill.git
 ```
+The extension will also soon be available on the Chrome Web Extension Store.
 
 # To Use
 While running a live server for the ```index.html``` file, navigate to the Chrome browser's extensions tab and ```Load Unpack``` the **Quick Fill** project.
-When accessing any Workday Account registration web page, simply select the autofill option.
+When accessing any Workday Account registration web page, simply save and then select the autofill option.
 
 Finally, all user information is strictly stored on Chrome's Local Storage asynchronously. Overall, this extension should generally only be used to speed up the job application and account creation process with Workday.
+
+# Contributions
+Contributions are always welcome. Simply fork the repository and create a pull request with a short description of the changes being made.
--- document.js
+++ document.js
@@ -1,84 +1,102 @@
-
 // Auto filling webpage 
-function autoFillForm(savedEmail, savedPassword){
+function autoFillForm(savedEmail, savedPassword) {
 
 
-        // Querying Input Labels
-        let emailInput = document.querySelector('input[data-automation-id="email"]');
-        let firstPassword = document.querySelector('input[data-automation-id="password"]');
-        let secondPassword = document.querySelector('input[data-automation-id="verifyPassword"]');
+    // Querying Input Labels
+    let emailInput = document.querySelector('input[data-automation-id="email"]');
+    let firstPassword = document.querySelector('input[data-automation-id="password"]');
+    let secondPassword = document.querySelector('input[data-automation-id="verifyPassword"]');
 
-        if (emailInput) {
-            
-            setTimeout(() => {
-                emailInput.value = savedEmail; 
+    if (emailInput) {
 
-                emailInput.dispatchEvent(new Event('input', { bubbles: true }));
-                emailInput.dispatchEvent(new Event('change', { bubbles: true }));
-                emailInput.dispatchEvent(new Event('blur', { bubbles: true }));
-                },500 );
+        setTimeout(() => {
+            emailInput.value = savedEmail;
 
-        }
-        if (firstPassword){
+            emailInput.dispatchEvent(new Event('input', {
+                bubbles: true
+            }));
+            emailInput.dispatchEvent(new Event('change', {
+                bubbles: true
+            }));
+            emailInput.dispatchEvent(new Event('blur', {
+                bubbles: true
+            }));
+        }, 500);
 
-            firstPassword.value = savedPassword;
+    }
+    if (firstPassword) {
 
-            firstPassword.dispatchEvent(new Event('input', { bubbles: true }));
-            firstPassword.dispatchEvent(new Event('change', { bubbles: true }));
-            firstPassword.dispatchEvent(new Event('blur', { bubbles: true }));
-        }
+        firstPassword.value = savedPassword;
 
-        if(secondPassword){
+        firstPassword.dispatchEvent(new Event('input', {
+            bubbles: true
+        }));
+        firstPassword.dispatchEvent(new Event('change', {
+            bubbles: true
+        }));
+        firstPassword.dispatchEvent(new Event('blur', {
+            bubbles: true
+        }));
+    }
+
+    if (secondPassword) {
 
 
-            setTimeout(() => {
-            
+        setTimeout(() => {
+
             secondPassword.value = savedPassword;
 
-            secondPassword.dispatchEvent(new Event('input', { bubbles: true }));
-            secondPassword.dispatchEvent(new Event('change', { bubbles: true }));
-            secondPassword.dispatchEvent(new Event('blur', { bubbles: true }));
+            secondPassword.dispatchEvent(new Event('input', {
+                bubbles: true
+            }));
+            secondPassword.dispatchEvent(new Event('change', {
+                bubbles: true
+            }));
+            secondPassword.dispatchEvent(new Event('blur', {
+                bubbles: true
+            }));
 
 
-            },500);
-        }
-   
+        }, 500);
+    }
+
 }
 
 
 // Listener that responds to AutoFill button
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
+    function (request, sender, sendResponse) {
 
         if (request.action === "autofill") {
-            chrome.storage.sync.get(['email', 'password', 'pressedSave'], function(items) {
-                
+            chrome.storage.sync.get(['email', 'password', 'pressedSave'], function (items) {
+
                 // Auto Filling form only if save is pressed
-                if ( items.pressedSave == true ){
-                    const savedEmail = items.email; 
+                if (items.pressedSave == true) {
+                    const savedEmail = items.email;
                     const savedPassword = items.password;
                     autoFillForm(savedEmail, savedPassword);
                 }
-                
-                sendResponse({ result: "Completed" });
+
+                sendResponse({
+                    result: "Completed"
+                });
             });
-        
-        }    
-        
+
+        }
+
         return true;
     }
 );
 
 
 
-chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'https://icf.wd5.myworkdayjobs.com/en-US/ICFExternal_Career_Site/job/Albany%2C-NY/Associate-Software-Engineer_R2403756/apply/autofillWithResume?utm_source=cvrve'},
-      })
-      ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {
+                hostSuffix: '.workdayjobs.com'
+            },
+        })],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
-  });
-
-
+});
